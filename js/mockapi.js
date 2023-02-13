985e91f2a5065e90a5b1d2c213c7816d5a677bbd
agonzalezrando <93986162+AlvaroCesur@users.noreply.github.com>
2023-02-13 18:31:50 +0100

    Cambiada

--- js/mockapi.js
+++ js/mockapi.js
@@ -25,7 +25,15 @@ fetch("https://63d158563f08e4a8ff95f787.mockapi.io/menu")
         let nuevoplato = plantilla.content.cloneNode(true);
         nuevoplato.querySelector("img").src = el.picture;
         nuevoplato.querySelector("h4").innerText = el.name;
-        nuevoplato.querySelector("p").innerText = el.ingredients;
+
+        function ingredients(ingredients) {
+          if (ingredients.length <= 1) {
+            return ingredients[0] + ".";
+          }
+          return ingredients.slice(0, -1).join(", ") + " y " + ingredients.at(-1) + ".";
+        }
+
+        nuevoplato.querySelector("p").innerText = ingredients(el.ingredients);
         wrap.appendChild(nuevoplato);        
         
         contenedor.appendChild(wrap);  
